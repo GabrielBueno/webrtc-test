8a29d7124b5d3a7eb089cd2da95d0751a520a9cb
GabrielBueno <gabrielbueno99@outlook.com>
2021-04-08 09:57:05 -0300

    deleted gosfu.exe

--- server/go_sfu/go.mod
+++ server/go_sfu/go.mod
@@ -3,6 +3,7 @@ module gosfu
 go 1.16
 
 require (
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/pion/rtcp v1.2.6 // indirect
 	github.com/pion/webrtc/v3 v3.0.20 // indirect
 )
--- server/go_sfu/main.go
+++ server/go_sfu/main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	// "github.com/pion/rtcp"
 	"github.com/pion/webrtc/v3"
+	"github.com/gorilla/websocket"
 )
 
+type Peer struct {
+	name string
+	conn *webrtc.PeerConnection
+}
+
 type Room struct {
-	name                 string
-	broadcasterPeerConn *webrtc.PeerConnection
+	name        string
+	broadcaster Peer
+	watchers    []Peer
+}
+
+var upgrader = websocket.Upgrader{}
+
+func echo(res http.ResponseWriter, req *http.Request) {
+
 }
 
 func main() {
